8b21665447bde9490daae49cfd611d63a7b88d79
Elijah Balogun <ebalogun@csgs-mac-mini.civilisations.ca>
2018-06-25 10:05:23 -0700

    fixes

--- server.js
+++ server.js
@@ -9,9 +9,8 @@ const app = express();
 
 
 const path = require('path');
-app.get('/*all', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
-
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
