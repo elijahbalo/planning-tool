c2ad6e0eadbaed71a62cb038d292bccc28b0dff0
Elijah Balogun <ebalogun@csgs-mac-mini.civilisations.ca>
2018-06-25 11:14:16 -0700

    fixes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//Install express server
 const express = require('express');
 const path = require('path');
 
@@ -6,9 +7,10 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
