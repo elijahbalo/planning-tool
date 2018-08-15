02c84db404ad187b0aa33180ea8ced16e52e4c82
Elijah Balogun <ebalogun@csgs-mac-mini.civilisations.ca>
2018-08-15 14:25:59 -0700

    changes

--- src/pages/landing-page/landing-page.component.ts
+++ src/pages/landing-page/landing-page.component.ts
@@ -19,16 +19,25 @@ import 'rxjs/add/observable/throw';
 })
 export class LandingPageComponent implements OnInit {
 
+
+first:boolean
+second:boolean
+
+
+
  
   p: number = 1;
 
   log = '';
   KinderTo2: boolean;
+  Gr3To5: boolean;
+  Gr6To8: boolean;
   checkboxValue:boolean;
   closeResult: string;
   items: any
-  filtered: any;
+  filtered:any;
   filters = {}
+  filters1 = {}
   grades: string;
   timeOfYear: string;
   day: string;
@@ -40,13 +49,12 @@ export class LandingPageComponent implements OnInit {
     localStorage.setItem('submit', JSON.stringify(false));
     localStorage.setItem('design', JSON.stringify(false));
     localStorage.setItem('confirm', JSON.stringify(false));
-    this.items = this.db.collection('/itineraries').valueChanges().subscribe(items => {
+   this.db.collection('/itineraries').valueChanges().subscribe(items => {
       this.items=items
       this.applyFilters()
     })
     localStorage.setItem("set",JSON.stringify(false))
-    console.log(this.items)
-  
+   console.log(this.items)
 
   }
 
@@ -76,38 +84,97 @@ export class LandingPageComponent implements OnInit {
 
 
   private applyFilters() {
+   
     console.log('action called')
-    if ((this.grades == "empty")){
-    this.filtered= this.items;
-    }
-    else{
-    this.filtered = Lodash.filter(this.items, Lodash.conforms(this.filters))
-    }
-    console.log(this.grades)
+    let newlyFilter = Lodash.filter(this.items, Lodash.conforms(this.filters))
+    console.log(newlyFilter)
+    this.filtered = newlyFilter;
+    /* if (this.filtered === undefined || this.filtered.length == 0) {
+      this.filtered = newlyFilter;
+      console.log(true)
+      console.log(this.filtered)
+      console.log(newlyFilter)
+  }
+  else{
+       console.log(false)
+
+  newlyFilter.map((elem)=> this.filtered.push(elem));
+  console.log(this.filtered)
+  console.log(newlyFilter)
+  } */
+
+
   
   }
 
-  private applyFilters1() {
+  private applyFilters1(first) {
+    this.first=first
     console.log('action called')
-    this.filtered = Lodash.filter(this.items, Lodash.conforms(this.filters))
-    console.log(this.filtered)
+    let newlyFilter = Lodash.filter(this.items, Lodash.conforms(this.filters1))
+    console.log(newlyFilter)
+    this.filtered = newlyFilter;
+    /* if (this.filtered === undefined || this.filtered.length == 0) {
+      this.filtered = newlyFilter;
+      console.log(true)
+      console.log(this.filtered)
+      console.log(newlyFilter)
+  }
+  else{
+       console.log(false)
+
+  newlyFilter.map((elem)=> this.filtered.push(elem));
+  console.log(this.filtered)
+  console.log(newlyFilter)
+  } */
+
+
+  
+  }
+  
+
+  private applyFilters2(second) {
+    this.second=second
+    console.log('action called')
+    let newlyFilter = Lodash.filter(this.items, Lodash.conforms(this.filters1))
+    console.log(newlyFilter)
+    this.filtered = newlyFilter;
+    /* if (this.filtered === undefined || this.filtered.length == 0) {
+      this.filtered = newlyFilter;
+      console.log(true)
+      console.log(this.filtered)
+      console.log(newlyFilter)
+  }
+  else{
+       console.log(false)
+
+  newlyFilter.map((elem)=> this.filtered.push(elem));
+  console.log(this.filtered)
+  console.log(newlyFilter)
+  } */
+
+
+  
   }
 
-  private applyFilters2() {
-    console.log('action called')
-    this.filtered = Lodash.filter(this.items, Lodash.conforms(this.filters))
-    console.log(this.filtered)
-  }
 
 
-  filterBoolean(property: string, rule: boolean) {
+
+  filterBoolean1(property: string, rule: boolean) {
     if (!rule) this.removeFilter(property)
     else {
       this.filters[property] = val => val
-      this.applyFilters()
+      this.applyFilters1(rule)
     }
   }
 
+
+  filterBoolean2(property: string, rule: boolean) {
+    if (!rule) this.removeFilter(property)
+    else {
+      this.filters1[property] = val => val
+      this.applyFilters2(rule)
+    }
+  }
 
   removeFilter(property: string) {
     delete this.filters[property]
@@ -123,7 +190,7 @@ export class LandingPageComponent implements OnInit {
     this.applyFilters()
   }
 
-  filterExact1(property: string, rule: any) {
+ /*  filterExact1(property: string, rule: any) {
     this.filters[property] = val => val == rule
     this.applyFilters1()
   }
@@ -131,12 +198,20 @@ export class LandingPageComponent implements OnInit {
   filterExact2(property: string, rule: any) {
     this.filters[property] = val => val == rule
     this.applyFilters2()
-  }
+  } */
 
   reset(){
     $('.cbox').prop('checked', false);
     $('.rad').prop('checked', false);
+    this.removeFilter("day")
+    this.removeFilter("KinderTo2")
+    this.removeFilter("Gr3To5")
+    this.removeFilter("Gr6To8")
     this.filtered= this.items;
+
   }
+
+
 }
+
 
